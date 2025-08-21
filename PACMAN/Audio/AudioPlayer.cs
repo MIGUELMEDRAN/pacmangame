f59b054b1c27ceac1168459bb4d5769417833501
Maria Alejandra Rivera Galindo <alejandra.rivera0724@jala.university>
2025-08-21 09:44:56 -0500

    feat: agregar clase AudioPlayer.cs con logica de audio.

---
+++ PACMAN/Audio/AudioPlayer.cs
@@ -0,0 +1,55 @@
+using LibVLCSharp.Shared;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PACMAN.Audio;
+
+public class AudioPlayer : IDisposable
+{
+    private readonly LibVLC libVLC;
+    private readonly MediaPlayer mediaPlayer;
+    private readonly string chompPath;
+    private readonly string deadPath;
+
+    public AudioPlayer()
+    {
+        Core.Initialize();
+        libVLC = new LibVLC("--quiet", "--no-xlib");
+        mediaPlayer = new MediaPlayer(libVLC);
+        
+        chompPath = Path.Combine(AppContext.BaseDirectory, "Assets", "Sound", "chomp.mp3");
+        deadPath = Path.Combine(AppContext.BaseDirectory, "Assets", "Sound", "dead.wav");
+    }
+
+    public void Chomp()
+    {
+        Play(chompPath);
+    }
+
+    public void Dead()
+    {
+        Play(deadPath);
+    }
+
+    private void Play(string path)
+    {
+        Task.Run(() =>
+        {
+            if (mediaPlayer.IsPlaying)
+            {
+                mediaPlayer.Stop();
+            }
+            
+            using var media = new  Media(libVLC, path, FromType.FromPath);
+            mediaPlayer.Media = media;
+            mediaPlayer.Play();
+        });
+    }
+
+    public void Dispose()
+    {
+        mediaPlayer.Dispose();
+        libVLC.Dispose();
+    }
+}
