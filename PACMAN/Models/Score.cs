4500ffe08d1ec3eb5ebd3052fe3ae7a3196533b8
Maria Alejandra Rivera Galindo <alejandra.rivera0724@jala.university>
2025-08-21 11:42:15 -0500

    docs: agregar documentacion xml

--- PACMAN/Models/Score.cs
+++ PACMAN/Models/Score.cs
@@ -1,7 +1,18 @@
 namespace PACMAN.Models;
 
+/// <summary>
+/// Representa la puntuacion de un jugador.
+/// </summary>
 public class Score
 {
+    /// <summary>
+    /// Obtiene o estable la puntuacion mas alta del jugador
+    /// </summary>
     public int HighScore { get; set; }
+    
+    /// <summary>
+    /// Obtiene el nombre del jugador asociado.
+    /// Por defecto es "jugador".
+    /// </summary>
     public string PlayerName { get; set; } = "Jugador";
 }
